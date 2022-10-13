f0fe8f44562c307578a0a0ad16ba17e1d146d6e9
shosatojp <me@shosato.jp>
2022-10-13 04:07:27 +0000

    feature: search

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -5,8 +5,8 @@ export interface FetchResult {
     error: boolean
 }
 
-export const useFetchText: (url: string) => [string | null, FetchResult] = (url: string) => {
-    const [data, setData] = React.useState<string | null>(null);
+export const useFetchText = <T = string>(url: string, mapfn?: (text: string) => T): [T | null, FetchResult] => {
+    const [data, setData] = React.useState<T | null>(null);
     const [error, setError] = React.useState<boolean>(false);
     const [loading, setLoading] = React.useState<boolean>(false);
 
@@ -15,8 +15,12 @@ export const useFetchText: (url: string) => [string | null, FetchResult] = (url:
         (async () => {
             const res = await fetch(url);
             if (Math.floor(res.status / 100) === 2) {
-                const _data = await res.text();
-                setData(_data);
+                const _data: string = await res.text();
+                if (mapfn) {
+                    setData(mapfn(_data));
+                } else {
+                    setData(_data as T);
+                }
             } else {
                 console.log('error');
                 setError(true);
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -7,22 +7,55 @@ import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
 
 import { CopyType, EmojiInfo } from '../types';
 import { convertSlug, groupBy } from '../utils';
 import { useFetchText } from '../hooks/useFetch';
 import { useSnackbar } from '../hooks/useSnackbar';
 import { EmojiCard } from '../components/EmojiCard';
-import { Backdrop, CircularProgress } from '@mui/material';
+import { Backdrop, CircularProgress, TextField } from '@mui/material';
+
+export interface EmojiListProps {
+    onHandleCopy: (emojiInfo: EmojiInfo) => void
+    onGetCopyText: (emojiInfo: EmojiInfo) => string | null
+    emojiList: EmojiInfo[]
+}
+
+export const EmojiList: React.FC<EmojiListProps> = (props: EmojiListProps) => {
+    const emojiGroups: EmojiInfo[][] = groupBy(props.emojiList, t => t.category);
+    return <div>
+        {emojiGroups.map(group => {
+            const groupName = group[0].category;
+            const groupNameSlug = convertSlug(groupName);
+            return <div key={groupName}>
+                <a href={'#' + groupNameSlug} style={{ textDecoration: 'none', color: 'unset' }}><h1 id={groupNameSlug}>{groupName}</h1></a>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    {group.map(e =>
+                        <EmojiCard
+                            key={e.emoji}
+                            emojiInfo={e}
+                            onClick={props.onHandleCopy}
+                            onGetCopyText={props.onGetCopyText}
+                        />
+                    )}
+                </div>
+            </div>;
+        })}
+    </div>;
+};
 
 export const ListPage: React.FC = () => {
-    const [emojiJson, { loading }] = useFetchText('./data/emoji.json');
-    const emojiList: EmojiInfo[] = emojiJson ? JSON.parse(emojiJson) : [];
-    const emojiGroups: EmojiInfo[][] = groupBy(emojiList, t => t.category);
+    const [emojiList, { loading }] = useFetchText('./data/emoji.json', (text) => JSON.parse(text) as EmojiInfo[]);
     const { pushSnackbar } = useSnackbar();
     const [copyType, setCopyType] = React.useState<CopyType>('emoji');
+    const [query, setQuery] = React.useState<string>('');
 
-    const getCopyText = (emojiInfo: EmojiInfo) => {
+    const emojiListFiltered = query.length > 0
+        ? emojiList?.filter(e => e.description.toLowerCase().includes(query)) ?? []
+        : emojiList;
+
+    const getCopyText = React.useCallback((emojiInfo: EmojiInfo) => {
         switch (copyType) {
             case 'emoji':
                 return emojiInfo.emoji;
@@ -33,9 +66,9 @@ export const ListPage: React.FC = () => {
             default:
                 return null;
         }
-    };
+    }, [copyType]);
 
-    const handleCopy = (emojiInfo: EmojiInfo) => {
+    const handleCopy = React.useCallback((emojiInfo: EmojiInfo) => {
         const text = getCopyText(emojiInfo);
         if (text) {
             pushSnackbar({ message: `copied "${text}"`, severity: 'success' });
@@ -43,62 +76,69 @@ export const ListPage: React.FC = () => {
         } else {
             console.error('failed to get copy text');
         }
-    };
+    }, [getCopyText]);
 
-    const convertLaTeXEmojiFullname = (emojiInfo: EmojiInfo) => {
+    const convertLaTeXEmojiFullname = React.useCallback((emojiInfo: EmojiInfo) => {
         return convertSlug(emojiInfo.description);
-    };
+    }, []);
 
     return <>
         <AppBar position="sticky">
             <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Emoji List
-                </Typography>
-                <div style={{ flexGrow: 1 }}></div>
-                <FormControl>
-                    <InputLabel id="demo-simple-select-label">Copy Type</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        value={copyType}
-                        label="copy type"
-                        onChange={(event: SelectChangeEvent) => {
-                            setCopyType(event.target.value as CopyType);
-                        }}
-                    >
-                        <MenuItem value={'emoji'} title='Emoji' >Emoji (😀)</MenuItem>
-                        <MenuItem value={'slug'}>Slug (<code>grinning-face</code>)</MenuItem>
-                        <MenuItem value={'latex'}>LaTeX (<code>\emoji&#123;grinning-face&#125;)</code></MenuItem>
-                    </Select>
-                </FormControl>
+                <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', width: '100%' }}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Emoji List
+                        </Typography>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <TextField
+                            color={'info'}
+                            // fullWidth
+                            label={<><SearchIcon fontSize='small'/>Search</>}
+                            variant="outlined"
+                            value={query}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setQuery(event.target.value)}
+                            sx={{ m: 1 }}
+                        />
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+                        <FormControl>
+                            <InputLabel id="demo-simple-select-label">Copy Type</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                value={copyType}
+                                label="copy type"
+                                onChange={(event: SelectChangeEvent) => {
+                                    setCopyType(event.target.value as CopyType);
+                                }}
+                            >
+                                <MenuItem value={'emoji'} title='Emoji' >Emoji (😀)</MenuItem>
+                                <MenuItem value={'slug'}>Slug (<code>grinning-face</code>)</MenuItem>
+                                <MenuItem value={'latex'}>LaTeX (<code>\emoji&#123;grinning-face&#125;)</code></MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
+                </div>
             </Toolbar>
         </AppBar>
-        {loading
-            ? <Backdrop open={true}>
+        {loading &&
+            <Backdrop open={true}>
                 <CircularProgress />
-            </Backdrop>
-            : emojiGroups.map(group => {
-                const groupName = group[0].category;
-                const groupNameSlug = convertSlug(groupName);
-                return <div key={groupName} style={{ display: 'flex', justifyContent: 'center' }}>
-                    <div style={{ margin: 10, maxWidth: 1500 }}>
-                        <a href={'#' + groupNameSlug} style={{ textDecoration: 'none', color: 'unset' }}><h1 id={groupNameSlug}>{groupName}</h1></a>
-                        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                            {group.map(e =>
-                                <EmojiCard
-                                    key={e.emoji}
-                                    emojiInfo={e}
-                                    onClick={handleCopy}
-                                    onGetCopyText={getCopyText}
-                                />
-                            )}
-                        </div>
-                    </div>
-                </div>;
-            })}
-
-        <p>
-            This site is powered by <a href='https://github.com/github/gemoji/blob/master/db/emoji.json'>GitHub emoji list</a>
-        </p>
+            </Backdrop>}
+        {!loading &&
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div style={{ margin: 10, maxWidth: 1500, width: '80vw' }}>
+                    {React.useMemo(() => <EmojiList
+                        onHandleCopy={handleCopy}
+                        onGetCopyText={getCopyText}
+                        emojiList={emojiListFiltered || []}
+                    />, [handleCopy, getCopyText, emojiListFiltered])}
+                    <p>
+                        This site is powered by <a href='https://github.com/github/gemoji/blob/master/db/emoji.json'>GitHub emoji list</a>
+                    </p>
+                </div>
+            </div>
+        }
     </>;
 };
